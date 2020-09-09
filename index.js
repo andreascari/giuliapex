4191e03331019ad9f7e695c03155d80e28c60594
Andrea <andrea.scaringello@hyperisland.se>
2020-09-09 12:10:29 +0100

    small screens in height queries, add listener for change of height

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+  window.addEventListener("resize", checkHeight);
+
+function checkHeight() {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-
+}
 
 
   function dogmode() {
